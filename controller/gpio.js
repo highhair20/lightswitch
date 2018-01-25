d07addcb598140bab07bed1d251c1a7e35159380
jason_inmarket <jason@inmarket.com>
2018-01-24 22:39:26 -0800

    ignore node_modules

---
+++ controller/gpio.js
@@ -0,0 +1,38 @@
+// ref: https://webofthings.org/2016/10/23/node-gpio-and-the-raspberry-pi/
+
+// https://nodejs.org/en/download/
+
+// Import the onoff library
+var onoff = require('onoff');
+// 
+var Gpio = onoff.Gpio;
+// declare an array to keep track of plug states 
+var pins = [];
+
+exports.initialize = function (pin) {
+  // Initialize the pin to be an output pin
+  pins[pin] = new Gpio(pin, 'out'); 
+}
+
+exports.toggle = function (pin) {
+  // Synchronously read the value of pin 4 and transform 1 to 0 or 0 to 1
+  var value = (pins[pin].readSync() + 1) % 2;
+  // Asynchronously write the new value to pin 4
+  plugs[pin].write(value, function() {
+    console.log("Changed pin " + pin + " state to: " + value);
+  });  
+}
+
+
+// Listen to the event triggered on CTRL+C
+process.on('SIGINT', function () {
+  // Cleanly close the GPIO pins before exiting
+  pins.forEach(function (pin, index) {
+    pin.writeSync(0);
+    pin.unexport();
+    console.log('Closed pin ' + pin);
+  });
+  console.log('Bye, bye!');
+  process.exit();
+});
+
